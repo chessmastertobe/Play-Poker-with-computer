965ee3199ed170061f262d49c581cd372078597b
Dennis Steele <onlinechessllc@gmail.com>
2022-07-17 03:18:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ import secrets
 importlib.reload(secrets)
 new_secret_key = 1234
 #new_secret_key = secrets.SUPER_SECRET_KEY
-dev_db_connection = secrets.DEVELOPMENT_DB
+dev_db_connection = 1234
+#dev_db_connection = secrets.DEVELOPMENT_DB
 test_db_connection = secrets.TEST_DB
 
 app = Flask(__name__, template_folder="Templates", static_folder="Static")
